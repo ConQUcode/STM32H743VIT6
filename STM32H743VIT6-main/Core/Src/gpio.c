46acb722c5672f0840f97522aeb1fe8f580f9f22
章铭宇 <15234223+zmyaaaz@user.noreply.gitee.com>
2026-07-10 01:01:09 +0800

    修改了光电门的gpio引脚

--- STM32H743VIT6-main/Core/Src/gpio.c
+++ STM32H743VIT6-main/Core/Src/gpio.c
@@ -90,8 +90,8 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : PE9 PE11 PE14 */
-  GPIO_InitStruct.Pin = GPIO_PIN_9|GPIO_PIN_11|GPIO_PIN_14;
+  /*Configure GPIO pins : PE10 PE11 PE12 PE14 */
+  GPIO_InitStruct.Pin = GPIO_PIN_10|GPIO_PIN_11|GPIO_PIN_12|GPIO_PIN_14;
   GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(GPIOE, &GPIO_InitStruct);
